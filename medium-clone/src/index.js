16d2609c51d86a6cb369ed142f2b166670e2a3f5
Vitalii Agapov <vitaly.agapov@outlook.com>
2020-01-02 10:48:38 -0500

    added routes

--- medium-clone/src/index.js
+++ medium-clone/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import Routes from "routes"
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+const App = () => {
+    return (
+        <div>
+            <h3>Welcome to hooks</h3>
+            <Router>
+                <Routes />
+            </Router>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
